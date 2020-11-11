25ceb2e822f3ae95654ea1e3587e9195414e53c8
kevinyohe <kevinyohe7@yahoo.com>
2020-11-11 14:03:07 -0500

    Fix

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -5,16 +5,18 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @Controller
 public class BlogController {
     private PostRepository postRepository;
     public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
     }
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
-        //List<Post> allPosts = new ArrayList<>(postRepository.getAllPosts());
-        //modelMap.put("posts",allPosts);
+        List<Post> allPosts = postRepository.getAllPosts();
+        modelMap.put("posts",allPosts);
         return "home";
     }
 }
